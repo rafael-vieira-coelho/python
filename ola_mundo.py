50a465619dc1b5e40d0dc59029869bb42b1fb91a
Rafael Vieira Coelho <rafaelvc2@gmail.com>
2020-03-17 09:00:03 -0300

    9 - Depuração

---
+++ ola_mundo.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# coding: utf-8
+
+''' Programa: Olá Mundo
+Descrição: Este programa serve de exemplo para iniciantes em python.'''
+
+__author__ = "Rafael Vieira Coelho"
+__license__ = "GPL"
+__version__ = "1.0"
+__email__ = "rafael.coelho@farroupilha.ifrs.edu.br"
+
+def main():
+	print("Olá mundo!")
+
+main()
